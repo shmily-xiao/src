bc7816fd95e58a41fab3b0df5efa842c05df3d35
wangzaijun <wzj@simpletour.com>
2016-11-03 14:34:00 +0800

    三种方法给逻辑加锁

--- main/java/com/thread/Test01.java
+++ main/java/com/thread/Test01.java
@@ -6,11 +6,11 @@ import java.util.concurrent.Executors;
 /**
  * Created by simpletour_Jenkin on 2016/11/2.
  *
- * 1.第一种在run方法调用 存取的方法的时候调用 synchronized (account) { 需要锁住的代码 }
+ * 1.第一种在run方法调用 存取的方法的时候调用 synchronized (account) { 需要锁住的代码 }  (account 是对象，锁对象)
  *
- * 2.在存取的方法上添加关键字 synchronized
+ * 2.在存取的方法上添加关键字 synchronized  （锁方法）
  *
- * 3.在需要锁住的类里面添加显示的添加锁 如：
+ * 3.在需要锁住的类里面添加显示的添加锁 如： （锁代码）
  *   // 显示的锁机制，为每个银行账户创建一个锁对象，在存款操作进行加锁和解锁的操作
  *    private Lock accountLock = new ReentrantLock();
  *
