9cfa1f7f7e9d781143af511eb0d7392c738a9db3
wangzaijun <wzj@simpletour.com>
2016-11-04 16:08:45 +0800

    一个不是很严谨的三次握手小程序

--- main/java/com/echo/EchoClient.java
+++ main/java/com/echo/EchoClient.java
@@ -1,36 +1,143 @@
 package com.echo;
 
+import javafx.scene.control.RadioMenuItem;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
  * Created by simpletour_Jenkin on 2016/11/3.
  *
  * socket 就是一进一出 client.getInputStream() 和 client.getOutputStream()
+ *
+ * 第几次，是否通过认证，客户端随机数，服务端随机数，是否结束回话，内容
+ *
+ * 比如：
+ * 1,false,256,456,false,abc
+ *
+ *
  */
 public class EchoClient {
     public static void main(String[] args) throws Exception{
+        int TIMES = 0;
+        int IS_SUCCESS = 1;
+        int CLIENT_RANDOM = 2;
+        int SERVER_RANDOM = 3;
+        int IS_FINISHED = 4;
+        int CONTENT = 5;
+        int LENGTH = 6;
+
+        int clientRand = 0;
         // 建立连接
         Socket client = new Socket("localhost",6789);
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("请输入内容：");
-        // 输入要发送的内容
-        String msg = scanner.nextLine();
-        scanner.close();
+
+        PrintWriter pw1 = new PrintWriter(client.getOutputStream());
+
+        Random random2 = new Random();
+        clientRand = random2.nextInt(1000)+1;
+
+        String msg2 = "1,false,"+clientRand+",0,false, 你好";
+
         // 利用socket在这个通道里面发送内容
-        PrintWriter pw = new PrintWriter(client.getOutputStream());
-        pw.println(msg);
+        pw1.println(msg2);
         // 必须要flush才会发出去
-        pw.flush();
-        // 利用socket接收这个通道里面的内容
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        System.out.println(bufferedReader.readLine());
+        pw1.flush();
+
+        while(true){
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+            String serverMsg = br.readLine();
+            System.out.println(serverMsg);
+            if (serverMsg==null || serverMsg.isEmpty()){
+                PrintWriter pw = new PrintWriter(client.getOutputStream());
+
+                Random random = new Random();
+                clientRand = random.nextInt(1000)+1;
+
+                String msg = "1,false,"+clientRand+",0,false, 你好";
+
+                // 利用socket在这个通道里面发送内容
+                pw.println(msg);
+                // 必须要flush才会发出去
+                pw.flush();
+            }else {
+                String[] serverArguments = serverMsg.split(",");
+                if ("true".equals(serverArguments[IS_FINISHED])) break;
+                if (serverArguments.length != LENGTH ) break;
+                if ("true".equals(serverArguments[IS_SUCCESS])){
+                    PrintWriter pw = new PrintWriter(client.getOutputStream());
+
+                    String clientMsg = "这是握手后的第一句话,然后拜拜！";
+                    String msg = serverArguments[TIMES] + "," + serverArguments[IS_SUCCESS] + ","
+                            +serverArguments[CLIENT_RANDOM]+"," +serverArguments[SERVER_RANDOM]+",true,"+clientMsg;
+                    pw.println(msg);
+                    pw.flush();
+                    break;
+                }else {
+                    int times = Integer.parseInt(serverArguments[TIMES])+1;
+                    int tempClientRandom = Integer.parseInt(serverArguments[CLIENT_RANDOM]);
+                    int serverRandom = Integer.parseInt(serverArguments[SERVER_RANDOM]) + 1;
+                    if (tempClientRandom != (clientRand+1)){
+                        break;
+                    }
+                    PrintWriter pw = new PrintWriter(client.getOutputStream());
+                    String msg = times + "," + serverArguments[IS_SUCCESS] + "," + tempClientRandom+","+serverRandom+","
+                            +serverArguments[IS_FINISHED]+",我收到了你的信息，咱两通话吧";
+                    pw.println(msg);
+                    pw.flush();
+                }
+
+            }
+        }
+        client.close();
+
+//        //-------------------------- 第一次握手 ---------------------------//
+//        // 输入要发送的内容
+//        Random random = new Random();
+//        int clientRand = random.nextInt(1000);
+//
+//        String msg = "第一次握手："+client.getInetAddress().toString()+","+clientRand;
+//
+////        Scanner scanner = new Scanner(System.in);
+////        System.out.println("请输入内容：");
+////        String msg = scanner.nextLine();
+////        scanner.close();
+//
+//        // 利用socket在这个通道里面发送内容
+//        PrintWriter pw = new PrintWriter(client.getOutputStream());
+//        pw.println(msg);
+//        // 必须要flush才会发出去
+//        pw.flush();
+//        //-------------------------- 第一次握手 ---------------------------//
+//
+//
+//        //-------------------------- 第二次握手 ---------------------------//
+//        // 利用socket接收这个通道里面的内容
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+//        String serverMsg = bufferedReader.readLine();
+//        System.out.println(serverMsg);
+//        String[] server = serverMsg.split(",");
+//        int validClientRand = Integer.parseInt(server[server.length-2]); // 约定好倒数第二个是客户端的随机数
+//        int serverRand = Integer.parseInt(server[server.length-1]); // 约定好最后一个是服务端的随机数
+//        if (validClientRand != clientRand+1){
+//            System.out.println("第二次握手失败，client随机数校验失败");
+//            client.close();
+//        }
+//        msg = "第二次握手：" +client.getInetAddress().toString()+"," +(serverRand+1);
+//        pw.println(msg);
+//        pw.flush();
+//        //-------------------------- 第二次握手 ---------------------------//
+//
+//        System.out.println(bufferedReader.readLine());
+
 
         // 关闭连接
-        client.close();
+//        client.close();
     }
 }
--- main/java/com/echo/EchoServer.java
+++ main/java/com/echo/EchoServer.java
@@ -1,11 +1,14 @@
 package com.echo;
 
+import org.apache.logging.log4j.core.jmx.Server;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Random;
 
 /**
  * Created by simpletour_Jenkin on 2016/11/3.
@@ -29,6 +32,13 @@ public class EchoServer {
     }
     private static class ClientHandler implements Runnable{
         private Socket client;
+        int TIMES = 0;
+        int IS_SUCCESS = 1;
+        int CLIENT_RANDOM = 2;
+        int SERVER_RANDOM = 3;
+        int IS_FINISHED = 4;
+        int CONTENT = 5;
+        int LENGTH = 6;
 
         public ClientHandler() {
         }
@@ -43,14 +53,43 @@ public class EchoServer {
             try(BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
                 // 在通道里面发送想发送的数据
                 PrintWriter pw = new PrintWriter(client.getOutputStream())){
-                // 得到接收过来的数据
-                String msg = br.readLine();
+                int serverRandom=0;
+                while (true){
+                    // 得到接收过来的数据
+                    String msg = br.readLine();
+                    System.out.println(client.getInetAddress() +" 发来信息 ："+ msg);
+                    if (msg==null || msg.isEmpty())continue;
+                    String[] clientArguments = msg.split(",");
+//                    if (clientArguments.length != LENGTH){
+//                        System.out.println(client.getInetAddress() +"---参数不合法与其断开连接");
+//                        break;
+//                    }
+                    if ("true".equals(clientArguments[IS_FINISHED])){
+                        System.out.println(client.getInetAddress() +"---主动断开连接");
+                        break;
+                    }
+                    int times = Integer.parseInt(clientArguments[TIMES]) +1;
+                    int clientRandom = Integer.parseInt(clientArguments[CLIENT_RANDOM])+1;
+                    int tempServerRandom = Integer.parseInt(clientArguments[SERVER_RANDOM]);
+                    if (times==2 && "false".equals(clientArguments[IS_FINISHED])){
+                        serverRandom = new Random().nextInt(1000)+1;
+                        String serverMsg = times+",false,"+clientRandom+","+serverRandom+","+clientArguments[IS_FINISHED]+",我收到了你的信息，我需要验证你的信息";
+                        System.out.println(serverMsg);
+                        pw.println(serverMsg);
+                        pw.flush();
+                    }else if (times==4&&"false".equals(clientArguments[IS_FINISHED])){
 
-                System.out.println("收到" + client.getInetAddress() + "发送的"+ msg);
+                        if (tempServerRandom != (serverRandom+1)){
+                            System.out.println(client.getInetAddress() +"---4参数不合法与其断开连接");
+                            break;
+                        }
+                        String serverMsg = times+",true,"+clientRandom+","+serverRandom+","+clientArguments[IS_FINISHED]+",我通过了你的信息，我们交流吧";
+                        System.out.println(serverMsg);
+                        pw.println(serverMsg);
+                        pw.flush();
+                    }
+                }
 
-                // 发送数据
-                pw.println(msg);
-                pw.flush();
             }catch (IOException e){
                 e.printStackTrace();
             }
